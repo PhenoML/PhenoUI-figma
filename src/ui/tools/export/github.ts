7dda58ba1bdc294dd1008f8fcba88bc8d858fdb2
Dario Segura <dario.seco@gmail.com>
2024-02-29 23:21:06 -0500

    [GITHUB] Properly uploads images

--- src/ui/tools/export/github.ts
+++ src/ui/tools/export/github.ts
@@ -1,6 +1,7 @@
 import {MessageBus} from "../../../shared/MessageBus";
 import {Github, GithubFile} from "../../Github";
 import {extractImages} from "./export";
+import {Octokit} from 'octokit';
 
 export async function commitToGithub(bus: MessageBus, name: string, payload: any, github: Github) {
     if (!await github.isLoggedIn) {
@@ -8,14 +9,36 @@ export async function commitToGithub(bus: MessageBus, name: string, payload: any
         return;
     }
 
-    const [type, folder] = payload.type === 'figma-component' ? ['Widget', 'widgets'] : ['Screen', 'screens'];
+    const [type, folder] = (() => {
+        switch (payload.type) {
+            case 'figma-component':
+                return ['Widget', 'widgets'];
+            case 'figma-image':
+                return ['Image', null];
+            default:
+                return ['Screen', 'screens'];
+        }
+    })();
 
     const files: GithubFile[] = [];
 
     const images = extractImages(payload);
     for (const image of images) {
-        const filename = `${image.name}.${image.format}`;
+        const uploadMethod = image.__userData.method ?? image.uploadMethod ?? 'embed';
+        // if the image method is set to embed, skip uploading
+        if (uploadMethod === 'embed') {
+            continue;
+        }
+
+        const filename = `${image.__info.name}.${image.format}`;
         const path = `assets/images/${filename}`;
+        const exists = await github.fileExists(path);
+
+        if (uploadMethod === 'link' && exists) {
+            image.data = path;
+            continue;
+        }
+
         files.push({
             path,
             content: image.data,
@@ -24,13 +47,24 @@ export async function commitToGithub(bus: MessageBus, name: string, payload: any
         image.data = path;
     }
 
-    files.push({
-        path: `${folder}/${name}.json`,
-        content: JSON.stringify(payload, null, 2),
-    });
+    if (folder) {
+        files.push({
+            path: `${folder}/${name}.json`,
+            content: JSON.stringify(payload, null, 2),
+        });
+    }
 
     const message = `[${github._user?.login.toUpperCase()}] ${type}: ${name}`;
 
-    await github.commitFiles(files, message);
-
+    while (true) {
+        try {
+            await github.commitFiles(files, message);
+            break;
+        } catch (e: any) {
+            // if the status is 422, wait for 10 seconds and try again
+            if (e.status === 422) {
+                await new Promise(resolve => setTimeout(resolve, 10000));
+            }
+        }
+    }
 }
